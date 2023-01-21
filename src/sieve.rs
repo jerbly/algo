ddcb0b71600f5d09e176baafaca9920e69c3a762
jerbly <jeremyblythe@gmail.com>
2023-01-20 22:13:02 -0500

    Simplified and non-loop extract

--- src/sieve.rs
+++ src/sieve.rs
@@ -7,75 +7,39 @@ Create a data structure with an array [0u64; 1024]. This is a bit vector which h
 4. Write a routine that extracts n bits starting at bit m into a u64. You may assume that n<=64 and n>0 and m+n<=65536.
    You may not assume anything else about n and m.
 */
-const MAXBITS: usize = 65536;
 struct PrimeBits {
     primes: [u64; 1024],
 }
 
 impl PrimeBits {
+    const MAXBITS: usize = 65536;
+    const SQRT_MAXBITS: usize = 256;
+
+    fn bit_div_mod64(n: u64) -> (u64, u64) {
+        let q = n >> 6;
+        let r = n - (q << 6);
+        (q, r)
+    }
+
     fn clear(&mut self, n: usize) {
-        let elem: usize = n / 64;
-        let bit: u64 = n as u64 % 64;
-        self.primes[elem] &= !(1 << bit);
+        let (elem, bit) = Self::bit_div_mod64(n as u64);
+        self.primes[elem as usize] &= !(1 << bit);
     }
 
     fn is_set(&self, n: usize) -> bool {
-        let elem: usize = n / 64;
-        let bit: u64 = n as u64 % 64;
+        let (elem, bit) = Self::bit_div_mod64(n as u64);
         let mask: u64 = 1 << bit;
-        self.primes[elem] & mask == mask
+        self.primes[elem as usize] & mask == mask
     }
 
     /// Return true/false whether n is prime or not.
     // You may not use divison, mod or multiplication in your solution.
     fn is_prime(&self, n: usize) -> Option<bool> {
-        if n >= MAXBITS {
+        if n >= Self::MAXBITS {
             None
         } else {
-            let (elem, bit) = Self::bit_div(n as u64, 64);
-            let mask: u64 = 1 << bit;
-            Some(self.primes[elem as usize] & mask == mask)
+            Some(self.is_set(n))
         }
-    }
-
-    /// recursive bitwise division
-    fn bit_div(dividend: u64, divisor: u64) -> (u64, u64) {
-        Self::division(dividend, divisor, divisor, 0)
-    }
-
-    fn division(dividend: u64, mut divisor: u64, origdiv: u64, mut remainder: u64) -> (u64, u64) {
-        println!(
-            "Entered: dd={:08b},{:3} dv={:08b},{:3}",
-            dividend, dividend, divisor, divisor
-        );
-
-        let mut quotient: u64 = 1;
-        if dividend == divisor {
-            return (1, 0);
-        } else if dividend < divisor {
-            return (0, dividend);
-        }
-
-        while divisor <= dividend {
-            divisor <<= 1;
-            quotient <<= 1;
-        }
-
-        if dividend < divisor {
-            divisor >>= 1;
-            quotient >>= 1;
-        }
-
-        println!(
-            "Leaving: dd={:08b},{:3} dv={:08b},{:3} qu={:08b},{:3}",
-            dividend, dividend, divisor, divisor, quotient, quotient
-        );
-
-        let (q, r) = Self::division(dividend - divisor, origdiv, origdiv, remainder);
-        quotient += q;
-        remainder = r;
-
-        (quotient, remainder)
     }
 
     /// number of primes
@@ -88,16 +52,22 @@ impl PrimeBits {
     }
 
     /// extracts n bits starting at bit m into a u64
-    fn extract(&self, m: usize, n: usize) -> u64 {
-        let mut result = 0u64;
-        let mut i = 0;
-        while i < n {
-            if self.is_set(m + i) {
-                result |= 1 << i;
-            }
-            i += 1;
+    fn extract(&self, m: u64, n: u64) -> u64 {
+        let (elem1, bit1) = Self::bit_div_mod64(m);
+        let (elem2, bit2) = Self::bit_div_mod64(m + n);
+        if elem1 == elem2 {
+            // section from a single element
+            let mut result = self.primes[elem1 as usize];
+            result <<= 64 - bit2;
+            return result >> (64 - n);
         }
-        result
+        // section split across two element
+        let mut back = self.primes[elem1 as usize];
+        back >>= bit1;
+        let mut result = self.primes[elem2 as usize];
+        result <<= 64 - bit2;
+        result >>= 64 - n;
+        result | back
     }
 
     /// Sieve of Eratosthenes: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
@@ -107,10 +77,10 @@ impl PrimeBits {
         };
         p.clear(0);
         p.clear(1);
-        for i in 2..MAXBITS {
+        for i in 2..Self::SQRT_MAXBITS {
             if p.is_set(i) {
                 let mut j = i * 2;
-                while j < MAXBITS {
+                while j < Self::MAXBITS {
                     p.clear(j);
                     j += i;
                 }
@@ -129,18 +99,10 @@ mod tests {
         let p = PrimeBits::new();
         assert_eq!(p.is_prime(199), Some(true));
         assert_eq!(p.is_prime(198), Some(false));
-        println!("{}", p.num_primes());
-        println!("{:064b} {:064b}", p.primes[0], p.primes[1]);
-        println!("{:08b}", p.extract(60, 8));
-    }
-    #[test]
-    fn test2() {
-        let dividend = 195;
-        let divisor = 7;
-
-        let (q, r) = PrimeBits::bit_div(dividend, divisor);
-        println!("{},{}", q, r);
-        assert_eq!(q, dividend / divisor);
-        assert_eq!(r, dividend % divisor);
+        assert_eq!(p.num_primes(), 6542);
+        assert_eq!(p.extract(60, 8), 0b1000_0010);
+        // println!("{:064b}", p.primes[0]);
+        // println!("{:064b}", p.primes[1]);
+        // println!("{:08b}", p.extract(60, 8));
     }
 }
